8349db124e5a912288f557441f3e0749a5312983
Weslei Almeida Muniz <weslei_almeida@yahoo.com.br>
2023-02-28 10:17:15 -0300

    Create Questão2-fibonnaci.js

---
+++ Questão2-fibonnaci.js
@@ -0,0 +1,22 @@
+function fibonacci(resp){
+  
+  var respUsuario = resp
+  var penultimo = 0;
+  var ultimo= 1;
+  var numero = 0;
+
+  for (let termo = 0; numero < respUsuario; termo++) {
+      numero= ultimo + penultimo;
+      penultimo = ultimo;
+      ultimo=numero;
+  }
+
+  if(numero === respUsuario){
+    return `O numero ${respUsuario} pertence sequência de Fibonacci`
+  } else {
+    return `O numero ${respUsuario} não pertence a sequência de Fibonacci`
+  }
+  
+}
+
+console.log(fibonacci(610));
